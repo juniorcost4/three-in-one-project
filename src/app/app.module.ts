28605468ff83766b81841bd5013b7f5dd721eee4
Délio Marcos <deliomarcos.contato@gmail.com>
2022-03-07 12:04:36 -0300

    Finalizando a base da aplicacao;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { PersonModule } from 'src/person/person.module';
+import { HobbyModule } from 'src/hobby/hobby.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://cirrus-dev:cirrus-dev@cluster0.twr4x.mongodb.net/three-in-one-db'),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    PersonModule,
+    HobbyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/hobby/hobby.model.ts
+++ src/hobby/hobby.model.ts
@@ -1,10 +1,14 @@
 import { Document, Schema as MongooseSchema } from 'mongoose';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
+@ObjectType()
 @Schema()
 export class Hobby {
+  @Field(() => String)
   _id: MongooseSchema.Types.ObjectId;
 
+  @Field(() => String)
   @Prop()
   name: string;
 }
